9b82c9ef4b1b3cca74228d1cc8d4baf551fd2989
Bryan Brinez <bryan-26-8@hotmail.com>
2023-09-08 18:25:25 -0500

    first commit

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,8 +1,58 @@
-import Register from "@/components/Register";
+"use client";
+import { useState } from "react";
 
+import Axios from "axios";
 
-export default function page() {
+export default function Page() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [fullname, setFullName] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(password);
+    try {
+      const res = await Axios.post("/api/auth/signup", {
+        email,
+        password,
+        fullname,
+      });
+    } catch (error) {
+      setError(error.response?.data.message); 
+    }
+  };
   return (
-    <Register/>
-  )
+    <div>
+      <form onSubmit={handleSubmit}>
+        {error && <div className="bg-red-500 text-white p-2 mb-2">{error}</div>}
+        <h1>Registrate</h1>
+        <input
+          className="bg-zinc-800 px-4 py-2 block mb-2"
+          type="text"
+          placeholder="John Doe"
+          name="fullname"
+          onChange={(e) => setFullName(e.target.value)}
+        />
+        <input
+          className="bg-zinc-800 px-4 py-2 block mb-2"
+          type="email"
+          placeholder="John@email.com"
+          name="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          className="bg-zinc-800 px-4 py-2 block mb-2"
+          type="password"
+          placeholder="********"
+          name="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button className="bg-indigo-500 px-4 py-2" type="submit">
+          {" "}
+          Registrar
+        </button>
+      </form>
+    </div>
+  );
 }
