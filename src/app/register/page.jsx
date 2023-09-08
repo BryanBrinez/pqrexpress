adb59d31669f6f7a950f72efecf93e239e2ba2f0
Bryan Brinez <bryan-26-8@hotmail.com>
2023-09-08 18:27:01 -0500

    el otro commit

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 
 import Axios from "axios";
 
-export default function Page() {
+export default function Page () {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [fullname, setFullName] = useState("");
