1d619e7f5284fa3d8bf2a3cb96e328198620c4c4
Bryan Brinez <bryan-26-8@hotmail.com>
2023-09-15 21:44:00 -0500

    homepage styles

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,33 +11,39 @@ export default function Home() {
       </div>
 
       <div className="flex-1 flex flex-col items-center justify-center">
-        <h1 className=" mb-4 text-4xl font-extrabold  leading-none tracking-tight text-colorThree md:text-5xl lg:text-6xl ">
-          Le damos la bienvenida al portal
-          <span className="text-blue-600 dark:text-blue-500"> PQREXPRESS</span>
-        </h1>
+        <div className="flex flex-col justify-center items-center mb-4">
+          <h1 className="text-4xl font-extrabold  leading-none tracking-tight text-colorThree md:text-5xl lg:text-6xl ">
+            Bienvenido al portal
+          </h1>
+          <span className="text-blue-600 leading-none tracking-tight text-4xl font-extrabold  dark:text-blue-500 ">
+            PQREXPRESS
+          </span>
+        </div>
 
-        <p className="text-lg font-normal mt-4 text-colorTwo lg:text-xl ">
-          Radique y haga seguimiento a las Peticiones, Quejas, Reclamos,
-          Sugerencias, Felicitaciones y Denuncias (PQR).
-        </p>
-        <p className="text-lg mt-6 text-colorThree font-semibold lg:text-xl">
-          ¿Qué desea realizar?
-        </p>
-        <div className="flex mt-4">
-          <Link
-            className="flex items-center mr-2 p-2 bg-colorOne text-white font-bold  rounded "
-            href="/crear-pqr"
-          >
-            <AiOutlineForm size={25} />
-            Crea tu PQR
-          </Link>
-          <Link
-            className="flex items-center p-2 bg-colorOne text-white font-bold  rounded "
-            href="/consultar-pqr"
-          >
-            <BiSearch size={25} />
-            Consulta tu PQR
-          </Link>
+        <div className="flex flex-col w-5/6 items-center justify-center">
+          <p className="text-lg font-normal mt-4  text-colorTwo lg:text-xl ">
+            Radique y haga seguimiento a las Peticiones, Quejas, Reclamos,
+            Sugerencias, Felicitaciones y Denuncias (PQR).
+          </p>
+          <p className="text-lg mt-6 text-colorThree font-semibold lg:text-xl">
+            ¿Qué desea realizar?
+          </p>
+          <div className="flex mt-4 w-full justify-between md:justify-around lg:justify-center lg:gap-4">
+            <Link
+              className="flex items-center p-2 bg-colorOne text-white font-bold rounded "
+              href="/crear-pqr"
+            >
+              <AiOutlineForm size={25} />
+              Crea tu PQR
+            </Link>
+            <Link
+              className="flex items-center p-2 bg-colorOne text-white font-bold  rounded "
+              href="/consultar-pqr"
+            >
+              <BiSearch size={25} />
+              Consulta tu PQR
+            </Link>
+          </div>
         </div>
       </div>
     </main>
