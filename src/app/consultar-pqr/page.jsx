6d54c92d5d235ebaa289c7c519f3483ca950fb02
Bryan Brinez <bryan-26-8@hotmail.com>
2023-09-16 20:41:56 -0500

    consultar/pqr/rad styles

--- src/app/consultar-pqr/page.jsx
+++ src/app/consultar-pqr/page.jsx
@@ -2,6 +2,8 @@
 import { useRouter } from "next/navigation";
 import Axios from "axios";
 import { useState, useEffect } from "react";
+import { MdArrowBack } from "react-icons/md";
+import Link from "next/link";
 
 export default function Page() {
   const [radicado, setRadicado] = useState(null);
@@ -31,18 +33,68 @@ export default function Page() {
       </div>
 
       <div className="flex-1 flex flex-col justify-center items-center">
+        <div className="flex w-full justify-start items-center">
+          <Link href={"/"}>
+            <MdArrowBack size={25} />
+          </Link>
+        </div>
+
         <h1 className="py-4 text-3xl font-extrabold  leading-none tracking-tight text-colorThree md:text-5xl lg:text-6xl ">
           Consultar solicitud
         </h1>
+
+        {error && (
+          <div
+            id="alert-2"
+            class="flex items-center p-4 mb-4 text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400"
+            role="alert"
+          >
+            <svg
+              class="flex-shrink-0 w-4 h-4"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+            </svg>
+            <span class="sr-only">Info</span>
+            <div class="ml-3 text-sm font-medium">
+              {error}
+            </div>
+            <button
+              type="button"
+              onClick={() => setError("")}
+              class="ml-auto -mx-1.5 -my-1.5 bg-red-50 text-red-500 rounded-lg focus:ring-2 focus:ring-red-400 p-1.5 hover:bg-red-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-red-400 dark:hover:bg-gray-700"
+              data-dismiss-target="#alert-2"
+              aria-label="Close"
+            >
+              <span class="sr-only">Close</span>
+              <svg
+                class="w-3 h-3"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 14"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                />
+              </svg>
+            </button>
+          </div>
+        )}
         <h2 className="text-lg font-semibold leading-none  tracking-tight text-colorThree md:text-5xl lg:text-6xl ">
           Ingrese el numero de su radicado del PQR
         </h2>
 
-        {error && <div className="bg-red-500 text-white p-2 mb-2">{error}</div>}
-
         <form className="py-6" onSubmit={handleSubmit}>
           <div className="relative z-0 w-full mb-6 group ">
-            <input
+            <input style={{borderColor: error ? 'red' : ''}}
               type="number"
               name="radicado"
               autoComplete="off"
