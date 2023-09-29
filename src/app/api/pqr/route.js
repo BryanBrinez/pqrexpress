1cafaed1e896b2861bb7d15b1ed7f32105593359
Felipe Buritica <andres.ruizburitica@gmail.com>
2023-09-28 22:55:41 -0500

    Change POST to generate random radicado number; change GET to get items with inversed order

--- src/app/api/pqr/route.js
+++ src/app/api/pqr/route.js
@@ -3,15 +3,23 @@ import { connectDB } from "@/libs/mongodb";
 import PQR from "@/models/pqr";
 
 export async function POST(request) {
-  const { fullname, email, number, departament, city, subject, description } =
+  const { fullname, email, number, departament, city, subject, description, type, mean } =
     await request.json();
   console.log(fullname, email, number, departament, city, subject, description);
+
+
+  function generarValorAleatorio() {
+    const min = 100000;
+    const max = 999999;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
 
   try {
     await connectDB();
 
     const listPQR = await PQR.find({});
-    const rad = listPQR.length + 1;
+    const rad = `${listPQR.length + 1}${generarValorAleatorio()}`;
      console.log(listPQR.length)
      console.log(rad)
 
@@ -25,6 +33,9 @@ export async function POST(request) {
       subject,
       description,
       response: "",
+      mean,
+      type,
+      status: "Pendiente",
     });
 
     const savedPQR = await newPQR.save();
@@ -48,7 +59,7 @@ export const GET = async (request) => {
   try {
       await connectDB()
 
-      const repuesto = await PQR.find({})
+      const repuesto = await PQR.find({}).sort({ createdAt: -1 })
 
       return new Response(JSON.stringify(repuesto), { status: 200 })
   } catch (error) {
@@ -59,12 +70,12 @@ export const GET = async (request) => {
 export const PUT = async (request) => {
   try {
     await connectDB();
-    const { radic, res} = await request.json();
+    const { radic, res, status} = await request.json();
 
     
 
     const filter = { radicado: radic };
-    const update = { response: res };
+    const update = { response: res, status: status };
    
     const pqrFound = await PQR.findOneAndUpdate(filter,update);
 
